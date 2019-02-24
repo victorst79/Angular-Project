e0fde2b54a910a93d5e21352a1907d4505895d33
unknown <victorst79@gmail.com>
2019-02-24 21:29:36 +0100

    add some functions and styles

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -8,16 +8,19 @@ import { HttpClient } from '@angular/common/http';
 export class NewsService {
   public news = [];
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
+    
+    // NEWS IN SPAIN
     this.http.get("https://newsapi.org/v2/everything?q=spain&apiKey=6d27df976d434e2e992f6ce34e4f9989")
     .subscribe(data => {
       var aux = data["articles"];
-      for(let i = 0; i <= aux.length; i++){
+      for(let i = 0; i <= aux.length-1; i++){
         this.news.push(aux[i]);
       }
     });
   }
   
+  // RETURN SPAIN NEWS
   getNews() {
     return this.news;
   }
