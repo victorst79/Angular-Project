d726d46f5dba314c24b1f206964abd245548281e
unknown <victorst79@gmail.com>
2019-02-27 00:50:34 +0100

    change pipe

--- src/app/limit-characters.pipe.ts
+++ src/app/limit-characters.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LimitCharactersPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    console.log(value);
     value = value.substr(0, 80);
     value += '...';
     return value;
