47e97db1490abde7a3cb91b9af9585015c2932e5
unknown <victorst79@gmail.com>
2019-02-27 19:26:37 +0100

    add details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: 'spain', component: MainComponent},
   { path: 'uk', component: MainComponent},
   { path: 'france', component: MainComponent},
+  { path: 'details', component: DetailsComponent},
   { path: '',   redirectTo: '/spain', pathMatch: 'full' },
   { path: '**', redirectTo: '/spain' }
 ];
